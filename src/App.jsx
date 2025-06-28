f266cdfc5cdb9b3169630eb3b1a9a0443733b89f
Harsh <harshmann8008@gmail.com>
2025-06-28 17:06:58 +0530

    feat: integrate Tailwind CSS and set up Redux store for authentication and added loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className=" flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* todo: <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col justify-center items-center h-screen w-full bg-gray-400">
+      <div
+        className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"
+        aria-label="Loading"
+      ></div>
+      <span className="mt-4 text-blue-700 font-semibold">Loading...</span>
+    </div>
+  );
 }
 
-export default App
+export default App;
