ad0baf8dfcaadd284d6fb3e311e3dba3f583aaa3
Harsh <harshmann8008@gmail.com>
2025-07-07 21:24:12 +0530

    update project name, add authentication components
    - add signup component
    - add login component
    - add postcard component
    - add select component

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,17 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
+import Select from "./Select";
+import Signup from "./SignUp";
 
-export { Footer, Header, Container, Logo, LogoutBtn, Button, Input };
+export {
+    Footer,
+    Header,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    Signup,
+};
